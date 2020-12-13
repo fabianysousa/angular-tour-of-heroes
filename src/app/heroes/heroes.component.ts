21a6b270f602e16c49f8c7a1381262dd326e7362
Fabiany Sousa <fabianydesousa@gmail.com>
2020-12-12 22:37:06 -0300

    refatorando o método getHeroes
    Agora o método getHeroes podera pegar dados de um servidor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,21 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
+  heroes: Hero[] = [];
   selectedHero!: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
